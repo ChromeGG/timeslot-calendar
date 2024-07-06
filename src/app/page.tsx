760a1d685eef3259c7ed11bc65cae4802d6ead85
Adam Tkaczyk <adam@tkaczyk.dev>
2024-07-07 00:07:18 +0200

    feat: :sparkles: add duration support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { NextUIProvider } from '@nextui-org/react'
 import { Button } from '@nextui-org/button'
 import { Input } from '@nextui-org/input'
+import { Select, SelectItem } from '@nextui-org/select'
 import { Card } from '@nextui-org/card'
 
 import FullCalendar from '@fullcalendar/react'
@@ -9,18 +10,45 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 
 import { useState } from 'react'
-import { DateSelectArg } from '@fullcalendar/core/index.js'
+import { DateSelectArg, EventInput } from '@fullcalendar/core/index.js'
+
+const durations = [
+  {
+    value: '00:15',
+    label: '15 minutes',
+  },
+  {
+    value: '00:30',
+    label: '30 minutes',
+  },
+  {
+    value: '01:00',
+    label: '1 hour',
+  },
+  {
+    value: '02:00',
+    label: '2 hours',
+  },
+  {
+    value: '03:00',
+    label: '3 hours',
+  },
+]
 
 export default function Home() {
-  const [events, setEvents] = useState<any>([])
+  const [name, setName] = useState('')
+  const [duration, setDuration] = useState(durations[0].value)
+  const [events, setEvents] = useState<EventInput[]>([])
   console.log('events:', events)
 
   const handleDateSelection = (arg: DateSelectArg) => {
     console.log('arg:', arg)
-    const newEvent = {
-      id: events.length + 1,
+    const newEvent: EventInput = {
+      id: String(events.length + 1),
       start: arg.start,
       end: arg.end,
+      overlap: false,
+      // TODO add constraint for today -> future only
     }
 
     setEvents([...events, newEvent])
@@ -30,10 +58,29 @@ export default function Home() {
     <NextUIProvider>
       <main>
         <Card className="m-12 p-12">
-          <h1 className="text-xl text-center px-4">Simple Time Slot</h1>
-          <Input className="my-4" label="Name of event" />
-          <div>Select time slots</div>
+          <h1 className="text-xl text-center px-4">Simple Time Slost</h1>
+          <Input
+            className="my-4"
+            label="Name of event"
+            isRequired
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Select
+            isRequired
+            label="Duration of Event"
+            // className="max-w-xs" if we want to limit the width
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+          >
+            {durations.map((duration) => (
+              <SelectItem key={duration.value} value={duration.value}>
+                {duration.label}
+              </SelectItem>
+            ))}
+          </Select>
 
+          <div>Select time slots</div>
           <div className="my-4">
             <FullCalendar
               plugins={[timeGridPlugin, interactionPlugin]}
@@ -41,7 +88,7 @@ export default function Home() {
               nowIndicator
               editable
               selectable
-              slotDuration="00:30"
+              slotDuration={duration}
               select={handleDateSelection}
               events={events}
             />
