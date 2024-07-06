fb18c4b5a398e90d4bce1fdc2d1b3b0e56ae046e
Adam Tkaczyk <adam@tkaczyk.dev>
2024-07-06 23:25:32 +0200

    feat: :sparkles: add possibility for timeslot selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,53 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import { enUS } from 'date-fns/locale'
-import { format, parse, startOfWeek, getDay } from 'date-fns'
+'use client'
 import { NextUIProvider } from '@nextui-org/react'
 import { Button } from '@nextui-org/button'
 import { Input } from '@nextui-org/input'
+import { Card } from '@nextui-org/card'
+
+import FullCalendar from '@fullcalendar/react'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+import { useState } from 'react'
+import { DateSelectArg } from '@fullcalendar/core/index.js'
 
 export default function Home() {
-  // const locales = {
-  //   'en-US': enUS,
-  // }
-  // const localizer = dateFnsLocalizer({
-  //   format: () => {},
-  //   parse : () => {},
-  //   startOfWeek : () => {},
-  //   getDay: () => {},
-  //   locales,
-  // })
+  const [events, setEvents] = useState<any>([])
+  console.log('events:', events)
+
+  const handleDateSelection = (arg: DateSelectArg) => {
+    console.log('arg:', arg)
+    const newEvent = {
+      id: events.length + 1,
+      start: arg.start,
+      end: arg.end,
+    }
+
+    setEvents([...events, newEvent])
+  }
+
   return (
     <NextUIProvider>
       <main>
-        <h1 className="text-lg text-red-500">Hello, world!</h1>
-        <Button>Click me</Button>
-        <Input label="Name of event" />
-        {/* <Calendar
-          localizer={localizer}
-          events={[]}
-          startAccessor="start"
-          endAccessor="end"
-          style={{ height: 500 }}
-        /> */}
+        <Card className="m-12 p-12">
+          <h1 className="text-xl text-center px-4">Simple Time Slot</h1>
+          <Input className="my-4" label="Name of event" />
+          <div>Select time slots</div>
+
+          <div className="my-4">
+            <FullCalendar
+              plugins={[timeGridPlugin, interactionPlugin]}
+              weekends
+              nowIndicator
+              editable
+              selectable
+              slotDuration="00:30"
+              select={handleDateSelection}
+              events={events}
+            />
+          </div>
+          <Button>Generate Link</Button>
+        </Card>
       </main>
     </NextUIProvider>
   )
