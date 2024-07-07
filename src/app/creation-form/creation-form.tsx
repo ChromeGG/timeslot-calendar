602b4b23f8f5286fcede639bd133370914fbf0a2
Adam Tkaczyk <adam@tkaczyk.dev>
2024-07-07 18:20:22 +0200

    feat: :sparkles: develop invitation selection page

--- src/app/creation-form/creation-form.tsx
+++ src/app/creation-form/creation-form.tsx
@@ -9,13 +9,27 @@ import type { DateSelectArg, EventInput } from '@fullcalendar/core/index.js'
 import { NameInput } from './name-input'
 import { DurationSelect } from './duration-select'
 
-// import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import z from 'zod'
+
+const creationFormSchema = z.object({
+  name: z.string().min(3).max(30),
+  duration: z.string(), // todo should match DurationOption values
+  timeSlots: z.object({
+    start: z.date(), // todo check if start date is before end date
+    end: z.date(), // todo check if start date is before end date
+  }),
+})
 
 export const CreationForm = () => {
   const [events, setEvents] = useState<EventInput[]>([])
   console.log('events:', events)
 
-  // const form = useForm()
+  const form = useForm({
+    resolver: zodResolver(creationFormSchema),
+  })
 
   const handleDateSelection = (selectedSlot: DateSelectArg) => {
     console.log('arg:', selectedSlot)
@@ -35,7 +49,7 @@ export const CreationForm = () => {
       <div className="my-6">
         <NameInput />
       </div>
-      <div className='my-6'>
+      <div className="my-6">
         <DurationSelect />
       </div>
 
@@ -51,7 +65,7 @@ export const CreationForm = () => {
           events={events}
         />
       </div>
-      <Button >Generate Link</Button>
+      <Button>Generate Link</Button>
     </>
   )
 }
--- src/app/invitation/[id]/page.tsx
+++ src/app/invitation/[id]/page.tsx
@@ -1,8 +1,15 @@
 'use client'
 import { Card } from '@nextui-org/card'
+import FullCalendar from '@fullcalendar/react'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { Button } from '@nextui-org/button'
+import { useState } from 'react'
 
 type Invitation = {
   id: string
+  name: string
+  duration: string
   events: []
 }
 
@@ -12,10 +19,37 @@ type InvitationPageProps = {
 
 export default function InvitationPage({ id }: InvitationPageProps) {
   console.log('invitation ID', id)
+  const [selected, useSelected] = useState()
   // TODO fetch by ID
+  const invitation = {
+    id: '123',
+    name: 'Test 123',
+    duration: '00:30',
+    events: [],
+  } satisfies Invitation
+
   return (
     <Card className="m-12 p-12">
-      <h1 className="text-xl text-center px-4">Invitation page</h1>
+      <h1 className="text-xl text-center">Choose delightful time slot</h1>
+      <div className="mt-4">
+        <p>Event name: {invitation.name}</p>
+        <p>Event duration: {invitation.duration}</p>
+        <div className="my-4">
+          <FullCalendar
+            plugins={[timeGridPlugin, interactionPlugin]}
+            weekends
+            nowIndicator
+            editable
+            selectable
+            // slotDuration={duration} // TODO get it from context
+            // select={handleDateSelection}
+            events={invitation.events}
+          />
+        </div>
+      </div>
+      <Button onClick={() => console.log('confirmed', selected)}>
+        Confirm Selection
+      </Button>
     </Card>
   )
 }
